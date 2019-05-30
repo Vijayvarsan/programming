b5e8d77195a8c218524be216776546eaede0d62e
Vijayvarsan <46055292+Vijayvarsan@users.noreply.github.com>
2019-05-30 11:30:48 +0530

    Update set1.3.py

--- set1.3.py
+++ set1.3.py
@@ -1,4 +1,4 @@
-ch=input("enter a character:")
+ch=input()
 vowels=['a','e','i','o','u']
 consonant=['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z']
 if ch in vowels:
